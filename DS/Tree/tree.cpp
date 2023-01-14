4b47108de8102901589f25091d97c6d4cd9c27dc
weiweichi <69968474+weiweichi@users.noreply.github.com>
2023-01-15 03:26:27 +0800

    modified tree

--- DS/Tree/tree.cpp
+++ DS/Tree/tree.cpp
@@ -8,10 +8,15 @@ struct TreeNode {
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    ~TreeNode() {delete left, right, val;};
 };
 
 struct nary_tree {
     int val;
     vector<nary_tree> child;
+    nary_tree() : val(0), child({}) {}
+    nary_tree(int x) : val(x), child({}) {}
+    nary_tree(int x, vector<nary_tree> &child) : val(x), child(child) {}
+    ~nary_tree() {;};
 };
 
